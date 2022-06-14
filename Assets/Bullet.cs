04855a0a7b46034552d11d2cead28f64229e6741
kobashi_hayato <k021g1128@g.neec.ac.jp>
2022-06-14 12:00:44 +0900

    敵が消滅してゲームクリアの画面まで

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -29,4 +29,15 @@ public class Bullet : MonoBehaviour
 
         }
     }
+    //当たり判定用関数
+    private void OnTriggerEnter(Collider other)
+    {
+        //もし当たったオブジェクトのタグがEnemyだったら
+        if (other.gameObject.tag == "Enemy")
+        {
+            //当たったオブジェクトのEnemyスクリプトを呼び出してDamage関数を実行させる
+            other.GetComponent<Enemy>().Damage();
+            Destroy(this.gameObject);
+        }
+    }
 }
