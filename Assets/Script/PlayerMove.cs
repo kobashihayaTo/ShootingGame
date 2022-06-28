d193988482345f263bd3ad9fd6eee20d2665d3da
kobashi_hayato <k021g1128@g.neec.ac.jp>
2022-06-28 12:25:56 +0900

    敵からいろいろな弾が出る処理を追加

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -20,25 +20,25 @@ public class PlayerMove : MonoBehaviour
         if(Input.GetKey(KeyCode.RightArrow))
         {
             //右方向に0.01動く
-            pos.x += 0.01f;
+            pos.x += 0.1f;
         }
         //左矢印キーが入力されたとき
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //左方向に0.01動く
-            pos.x -= 0.01f;
+            pos.x -= 0.1f;
         }
         //上矢印キーが入力されるとき
         if (Input.GetKey(KeyCode.UpArrow))
         {
             //上方向に0.01動く
-            pos.z += 0.01f;
+            pos.z += 0.1f;
         }
         //下矢印キーが入力されるとき
         if (Input.GetKey(KeyCode.DownArrow))
         {
             //下方向に0.01動く
-            pos.z -= 0.01f;
+            pos.z -= 0.1f;
         }
         transform.position = new Vector3(pos.x, pos.y, pos.z);
     }
